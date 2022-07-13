2f4910c5357d094189ce3a41f531b14aff798264
Bryant Beach <bryant.beach@libertymutual.com>
2022-07-13 16:20:26 -0400

    Adding initial python files pyblog, pyblog_utils, pylog_test

---
+++ python/pyblog_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import solar
+
+def test_get_observer_location():
+    val = get_observer_location()
+    assert val == '39.0438 -77.4874'
+
+def test_print_position():
+    val = print_position('123.45', '64.78')
+    assert val == 'The Sun is currently at: 123.45 degrees azimuth 64.78 degrees altitude'
