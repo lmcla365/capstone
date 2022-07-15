2b2a8c4bfafdb04370f7ca11f6c1bd27b93b1e3a
Bryant_Beach <101576438+n0157190@users.noreply.github.com>
2022-07-15 09:29:38 -0400

    Update pyblog_test.py
    adding additional test cases for pytest

--- python/pyblog_test.py
+++ python/pyblog_test.py
@@ -2,10 +2,29 @@
 
 import solar
 
-def test_get_observer_location():
-    val = get_observer_location()
-    assert val == '39.0438 -77.4874'
+def test_wphealthcheck():
+    val = wphealthcheck()
+    assert val == 200
+    
+def test_readpost():
+    val = readpost(1)
+    assert val == """Hello world!
+    Welcome to WordPress. This is your first post. Edit or delete it, then start writing!
+    """
 
-def test_print_position():
-    val = print_position('123.45', '64.78')
-    assert val == 'The Sun is currently at: 123.45 degrees azimuth 64.78 degrees altitude'
+def test_getposts():
+    val = getposts()
+    assert val == """Hello world!
+    Welcome to WordPress. This is your first post. Edit or delete it, then start writing!
+    """
+
+def test_listposts():
+    val = listposts()
+    assert val == """Post ID    Post Title
+    6       test post
+    1       Hello world!
+    """
+
+def test_writepost():
+    val = writepost('Test_Title','Test_Content')
+    assert val == 'Post written to Wordpress.'
