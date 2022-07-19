80347995dcdd22898c749b50b82c0506577502fe
Bryant_Beach <101576438+n0157190@users.noreply.github.com>
2022-07-19 09:54:18 -0400

    Update pyblog_test.py
    Adjust content for addpost and update post.  Updated content will allow consecutive runs of pytest.

--- python/pyblog_test.py
+++ python/pyblog_test.py
@@ -4,28 +4,69 @@ import pyblog_utils
 
 
 def test_wphealthcheck():
-    val = wphealthcheck()
+    val = pyblog_utils.wphealthcheck()
     assert val == 200
-    
+
+
+def test_readnonpost():
+    val = pyblog_utils.readpost(0)
+    assert val == "Post ID not valid."
+
+
 def test_readpost():
-    val = readpost(1)
-    assert val == """Hello world!
-    Welcome to WordPress. This is your first post. Edit or delete it, then start writing!
-    """
+    val = pyblog_utils.readpost(1)
+    assert (
+        val
+        == """Hello world!
+<p>Welcome to WordPress. This is your first post. Edit or delete it, then start writing!</p>
+"""
+    )
+
 
 def test_getposts():
-    val = getposts()
-    assert val == """Hello world!
-    Welcome to WordPress. This is your first post. Edit or delete it, then start writing!
-    """
+    val = pyblog_utils.getposts()
+    assert (
+        val
+        == """Hello world!
+<p>Welcome to WordPress. This is your first post. Edit or delete it, then start writing!</p>
+"""
+    )
 
-def test_listposts():
-    val = listposts()
-    assert val == """Post ID    Post Title
-    6       test post
-    1       Hello world!
-    """
+
+def test_searchpost():
+    val = pyblog_utils.searchpost("hello")
+    assert (
+        val
+        == """Hello world!
+<p>Welcome to WordPress. This is your first post. Edit or delete it, then start writing!</p>
+"""
+    )
+
 
 def test_writepost():
-    val = writepost('Test_Title','Test_Content')
-    assert val == 'Post written to Wordpress.'
+    f = open("infile.txt", "w")
+    f.write("""Hello world!
+<p>Welcome to WordPress. This is your first post. Edit or delete it, then start writing!</p>
+""")
+    f.close()
+    val = pyblog_utils.writepost("infile.txt")
+    assert val == "Post written to Wordpress."
+
+
+def test_updatepost():
+    f = open("update.txt", "w")
+    f.write("""Hello world!
+<p>Welcome to WordPress. This is your first post. Edit or delete it, then start writing!</p>
+""")
+    f.close()
+    postid = pyblog_utils.getlastpostid()
+    val = pyblog_utils.updatepost("update.txt", postid)
+    assert val == "Post updated in Wordpress."
+
+
+def test_postcomment():
+    postid = pyblog_utils.getlastpostid()
+    val = pyblog_utils.postcomment(
+        postid, "This is a comment", "student", "student@example.com"
+    )
+    assert val == "Comment written to Wordpress post."
