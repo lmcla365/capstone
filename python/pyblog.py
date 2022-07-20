0837010fb21394a136254b7e4fcd6095ee7d72e9
Bryant_Beach <101576438+n0157190@users.noreply.github.com>
2022-07-20 15:38:49 -0400

    Update pyblog.py
    refactor argument checks to account for args not accounted for.

--- python/pyblog.py
+++ python/pyblog.py
@@ -11,45 +11,42 @@ if pyblog_utils.wphealthcheck() != 200:
     print("Wordpress is currently down, check if site is running")
     exit()
 
-arglen = len(sys.argv)
-if arglen < 2 or sys.argv[1].lower() == "help":
-    print(
-        """    *** How to use pyblog ***
-    HELP:  python3 pyblog.py help
-    Upload a blogpost from file:  python3 pyblog.py upload -f <filename>
-    Update a blogpost from file by blog id:  python3 pyblog.py update -f <filename>  <id>
-    Show the latest blog post:  python3 pyblog.py latestpost
-    List all blog post:  python3 pyblog.py listposts')
-    Show specific blog post:  python3 pyblog.py readpost <post_id>
-    Post comment to a blog post:  python3 pyblog.py postcomment <post_id> "<comment>" <author name> <author email>"""
-    )
-    exit()
+def checkarg(arg1):
+    if arg1 not in ("upload","update","latestpost","listposts","searchpost","readpost","postcomment"):
+        print(
+            """    *** How to use pyblog ***
+        Upload a blogpost from file:  python3 pyblog.py upload -f <filename>
+        Update a blogpost from file by blog id:  python3 pyblog.py update -f <filename>  <id>
+        Show the latest blog post:  python3 pyblog.py latestpost
+        List all blog post:  python3 pyblog.py listposts
+        Search posts by keyword: python3 pyblog.py searchpost <keyword>
+        Show specific blog post:  python3 pyblog.py readpost <post_id>
+        Post comment to a blog post:  python3 pyblog.py postcomment <post_id> "<comment>" <author name> <author email>"""
+        )
+        exit()
+    else:
+        return True
 
-argv1 = sys.argv[1].lower()
-if arglen == 2:
+arglen = len(sys.argv)
+if arglen >= 2:
+    argv1 = sys.argv[1].lower()
+    checkarg(argv1)
     if argv1 == "latestpost":
         resp = pyblog_utils.getposts()
     elif argv1 == "listposts":
         resp = pyblog_utils.listposts()
-    else:
-        exit()
-if arglen == 3:
-    argv2 = sys.argv[2].lower()
-    if argv1 == "readpost":
+    elif argv1 == "readpost":
         resp = pyblog_utils.readpost(sys.argv[2])
     elif argv1 == "searchpost":
         resp = pyblog_utils.searchpost(sys.argv[2])
-    else:
-        exit()
-if arglen > 3:
-    if argv1 == "postcomment":
+    elif argv1 == "upload":
+        resp = pyblog_utils.writepost(sys.argv[3])
+    elif arglen == 5 and argv1 == "update":
+        resp = pyblog_utils.updatepost(sys.argv[4], sys.argv[2])
+    elif argv1 == "postcomment":
         resp = pyblog_utils.postcomment(
             str(sys.argv[2]), sys.argv[3], sys.argv[4], sys.argv[5]
         )
-    elif arglen == 4 and argv1 == "upload" and os.path.exists(sys.argv[3]):
-        resp = pyblog_utils.writepost(sys.argv[3])
-    elif arglen == 5 and argv1 == "update" and os.path.exists(sys.argv[4]):
-        resp = pyblog_utils.updatepost(sys.argv[4], sys.argv[2])
     else:
-        exit()
+        checkarg("help")
 print(resp)
